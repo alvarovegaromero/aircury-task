28f1eccdb3371b79f8d78ceaf31a8a3d3b5ed60c
Álvaro <vegaromeroalvaro@gmail.com>
2024-07-17 15:43:58 +0200

    input processing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Helper.getMessage());
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Write the name of the Puzzle you want to solve:");
+            String fileName  = scanner.nextLine();
+
+            File puzzle = new File("puzzles/"+fileName);
+
+            try (Scanner fileScanner = new Scanner(puzzle)) {
+                while (fileScanner.hasNextLine()) {
+                    String line = fileScanner.nextLine();
+                    System.out.println(line);
+                }
+                   
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred while reading the file: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred while reading the input: " + e.getMessage());
+        }
     }
 }
