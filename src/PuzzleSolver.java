e926869e29b12183d7371d8092a4382208624cb8
Álvaro <vegaromeroalvaro@gmail.com>
2024-07-18 13:56:36 +0200

    refactor done

--- src/PuzzleSolver.java
+++ src/PuzzleSolver.java
@@ -7,14 +7,15 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class PuzzleSolver {
+    private static final int ROTATIONS = 4;    
     private static int width, height;
-    private static List<Piece> pieces = new ArrayList<>();
-    private static List<int[][]> solutions = new ArrayList<>();
+    private static final List<Piece> pieces = new ArrayList<>();
+    private static final List<int[][]> solutions = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
-            String fileName = getFilenameInput();
-            processFile(fileName);
+            String fileName = getPuzzleFilenameFromUser();
+            loadPuzzleFromFile(fileName);
             solvePuzzle();
             displaySolutions();
         } catch(Exception e) {
@@ -22,7 +23,7 @@ public class PuzzleSolver {
         }
     }
 
-    private static String getFilenameInput() {
+    private static String getPuzzleFilenameFromUser() {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Write the name of the Puzzle you want to solve: ");
             String fileName = scanner.nextLine();
@@ -33,7 +34,7 @@ public class PuzzleSolver {
         }
     }
 
-    private static void processFile(String fileName) {
+    private static void loadPuzzleFromFile(String fileName) {
         File puzzle = new File("puzzles/" + fileName);
 
         try (Scanner fileScanner = new Scanner(puzzle)) { 
@@ -41,16 +42,15 @@ public class PuzzleSolver {
             height = fileScanner.nextInt();
             fileScanner.nextLine();
             
-            int i = 1;
+            int rowIndex = 1;
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine().trim();
                 if (!line.isEmpty()) {
                     String[] parts = line.split(" ");
                     int[] sides = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
-                    pieces.add(new Piece(sides, i));
-                    //pieces.add(generateAllRotations(sides));
+                    pieces.add(new Piece(sides, rowIndex));
                 }
-                i++;
+                rowIndex++;
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("The specified file could not be found: " + fileName);
@@ -58,184 +58,159 @@ public class PuzzleSolver {
     }
 
     private static void solvePuzzle() {
-        Piece[][] board = new Piece[height][width]; // Create an empty board
+        Piece[][] board = new Piece[height][width]; // empty board - all null
         
-        solveRecursive(board, new ArrayList<>(pieces), 0, 0); // Start solving the puzzle
+        solveRecursive(board, new ArrayList<>(pieces), 0, 0);
     }
         
     private static void solveRecursive(Piece[][] board, List<Piece> remainingPieces, int row, int col) {
-        //System.out.println("Solving - Row: "+ row+ " col: "+col );
-        
-        if (row == height) { // If we have filled the board.
-            System.out.println("FILLED THE BOARD");
-            int[][] solution = new int[height][width];
-            for (int i = 0; i < height; i++) {
-                for(int j = 0; j < width; j++) {
-                    solution[i][j] = board[i][j].getRowIndex();
-                }
-            }
-        
-            // Check if the solution is a rotation of an existing solution
-            boolean isRotation = false;
-            for (int[][] existingSolution : solutions) {
-                if (isRotation(solution, existingSolution)) {
-                    isRotation = true;
-                    break;
-                }
-            }
-        
-            // If the solution is not a rotation of an existing solution, add it
-            if (!isRotation) {
-                solutions.add(solution);
-            }
-        
-            return;
-        }
-        
-        if (col == width) { // If we have reached the end of a row
-            System.out.println("END OF A ROW - Row: "+ row+ " col: "+col );
-
-            for(int i = 0; i < row; i++){
-                for(int j = 0; j < width; j++)
-                {
-                    System.out.println(board[i][j].getRowIndex());
-                }
-            }
-
-            solveRecursive(board, remainingPieces, row + 1, 0); // Move to the next row
+        if (row == height) {
+            handlePossibleSolution(board);
             return;
         }
     
-        // Fill row by row
-        for (int i = 0; i < remainingPieces.size(); i++) {
-            Piece piece = remainingPieces.get(i);
+        if (col == width) { //End of current row. Move to next one
+            solveRecursive(board, remainingPieces, row + 1, 0);
+            return;
+        }
         
-            // Try all rotations of the piece
-            for (int rotation = 0; rotation < 4; rotation++) {
-                piece.rotate(); // Rotate the piece
+        // Fill column by column and once finished the row, move to the next one
+        tryAllPieces(board, remainingPieces, row, col);
+    }
+
+    private static void handlePossibleSolution(Piece[][] board) {
+        int[][] solution = new int[height][width];
         
-                if (pieceFits(board, piece, row, col)) { // If the piece fits
-                    placePiece(board, piece, row, col); // Place the piece on the board
-        
-                    // Remove the piece from the remaining pieces and continue with the next position
-                    List<Piece> newRemainingPieces = new ArrayList<>(remainingPieces);
-                    newRemainingPieces.remove(i);
-                    solveRecursive(board, newRemainingPieces, row, col + 1);
-        
-                    removePiece(board, piece, row, col); // No solution - prune - remove from board
-                }
+        for (int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                solution[i][j] = board[i][j].getRowIndex();
             }
+        }
+        
+        if (!isRotationOfExistingSolution(solution)) {
+            solutions.add(solution);
         }
     }
 
-    private static boolean isRotation(int[][] solution, int[][] existingSolution) {
-        for (int i = 0; i < 4; i++) {
-            if (Arrays.deepEquals(solution, existingSolution)) {
-                return true;
+    private static boolean isRotationOfExistingSolution(int[][] solution) {
+        for (int[][] existingSolution : solutions) {
+            for (int i = 0; i < ROTATIONS; i++) {
+                if (Arrays.deepEquals(solution, existingSolution)) {
+                    return true;
+                }
+                existingSolution = rotateMatrix(existingSolution);
             }
-            existingSolution = rotateMatrix(existingSolution);
         }
         return false;
     }
-    
+
     private static int[][] rotateMatrix(int[][] matrix) {
-        int size = matrix.length;
-        int[][] rotatedMatrix = new int[size][size];
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                rotatedMatrix[i][j] = matrix[size-j-1][i];
+        int[][] rotatedMatrix = new int[width][height];
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                rotatedMatrix[i][j] = matrix[width-j-1][i];
             }
         }
         return rotatedMatrix;
     }
 
+    private static void tryAllPieces(Piece[][] board, List<Piece> remainingPieces, int row, int col) {
+        for (int i = 0; i < remainingPieces.size(); i++) {
+            Piece piece = remainingPieces.get(i);
+    
+            for (int rotation = 0; rotation < ROTATIONS; rotation++) {
+                piece.rotate();
+    
+                if (pieceFits(board, piece, row, col)) {
+                    placePieceAndContinue(board, piece, remainingPieces, i, row, col);
+                    removePiece(board, row, col); // No solution found, remove piece
+                }
+            }
+        }
+    }
+
+    private static void placePieceAndContinue(Piece[][] board, Piece piece, List<Piece> remainingPieces, int i, int row, int col) {
+        placePiece(board, piece, row, col);
+    
+        List<Piece> newRemainingPieces = new ArrayList<>(remainingPieces);
+        newRemainingPieces.remove(i);
+        solveRecursive(board, newRemainingPieces, row, col + 1);
+    }
+
     private static boolean pieceFits(Piece[][] board, Piece piece, int row, int col) {
-        // Check if the piece fits within the limits of the board
-        //System.out.println("checking fit");
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
-            return false;
-        }
-    
-        // Check if the position on the board is already occupied
-        if (board[row][col] != null) {
+        if (!isWithinLimits(row, col) || isPositionOccupied(board, row, col)) {
             return false;
         }
 
-        // Check if the piece fits with the surrounding pieces
-        // Assuming that the Piece class has a getSides() method that returns an array of integers representing the sides
-        int[] sides = piece.getSides();
+        int [] sides = piece.getSides();
     
-        // Check above
+        return fitsAbove(board, sides[Orientations.UP.getValue()], row, col) &&
+               fitsRight(board, sides[Orientations.RIGHT.getValue()], row, col) &&
+               fitsBelow(board, sides[Orientations.DOWN.getValue()], row, col) &&
+               fitsLeft(board, sides[Orientations.LEFT.getValue()], row, col);
+    }
+
+    private static boolean isWithinLimits(int row, int col) {
+        return !(row < 0 || col < 0 || row >= height || col >= width);
+    }
+    
+    private static boolean isPositionOccupied(Piece[][] board, int row, int col) {
+        return board[row][col] != null;
+    }
+
+    private static boolean fitsAbove(Piece[][] board, int side, int row, int col) {
         if (row > 0) {
-            if (board[row - 1][col] != null && 
-                board[row - 1][col].getSides()[Orientations.DOWN.getValue()] 
-                != sides[Orientations.UP.getValue()]) {
-                return false;
-            }
-        } else if (sides[Orientations.UP.getValue()] != 0) { //row == 0
-            return false;
+            return board[row - 1][col] == null || //Piece above not null and with same side
+                board[row - 1][col].getSide(Orientations.DOWN.getValue()) == side;
+        } else { // row == 0
+            return side == 0;
         }
+    }
 
-        // Check right
-        if(col < (width - 1)){
-            if(board[row][col + 1] != null && 
-            board[row][col + 1].getSides()[Orientations.LEFT.getValue()] 
-            != sides[Orientations.RIGHT.getValue()]){
-                return false;
-            }
-        } else if(sides[Orientations.RIGHT.getValue()] != 0){ //col == width
-            return false;
+    private static boolean fitsRight(Piece[][] board, int side, int row, int col) {
+        if (col < (width - 1)) {
+            return board[row][col + 1] == null || 
+                   board[row][col + 1].getSide(Orientations.LEFT.getValue()) == side;
+        } else { // col == width - 1
+            return side == 0;
         }
-
-        // Check below
-        if(row < (height - 1)){
-            if(board[row + 1][col] != null && 
-            board[row + 1][col].getSides()[Orientations.UP.getValue()] 
-            != sides[Orientations.DOWN.getValue()]){
-                return false;
-            }
-        } else if(sides[Orientations.DOWN.getValue()] != 0){ //row == height
-            return false;
-        }
-
-        if(col > 0){
-            if(board[row][col - 1] != null && 
-            board[row][col - 1].getSides()[Orientations.RIGHT.getValue()] 
-            != sides[Orientations.LEFT.getValue()]){
-                return false;
-            }
-        } else if(sides[Orientations.LEFT.getValue()] != 0){ //col == 0
-            return false;
-        }
+    }
     
-        //System.out.print("PIECE FITS");
-
-        // If all checks passed, the piece fits
-        return true;
+    private static boolean fitsBelow(Piece[][] board, int side, int row, int col) {
+        if (row < (height - 1)) {
+            return board[row + 1][col] == null || 
+                   board[row + 1][col].getSide(Orientations.UP.getValue()) == side;
+        } else { // row == height - 1
+            return side == 0;
+        }
+    }
+    
+    private static boolean fitsLeft(Piece[][] board, int side, int row, int col) {
+        if (col > 0) {
+            return board[row][col - 1] == null || 
+                   board[row][col - 1].getSide(Orientations.RIGHT.getValue()) == side;
+        } else { // col == 0
+            return side == 0;
+        }
     }
 
     private static void placePiece(Piece[][] board, Piece piece, int row, int col) {
-        // Asumiendo que cada pieza tiene un ID único
-        System.err.println("piece PLACED - Piece: "+ piece.getRowIndex());
         board[row][col] = piece;
     }
     
-    private static void removePiece(Piece[][] board, Piece piece, int row, int col) {
-        // Asumiendo que un valor de 0 representa un espacio vacío en el tablero
-        System.err.println("piece REMOVED - Piece: "+ piece.getRowIndex());
+    private static void removePiece(Piece[][] board, int row, int col) {
         board[row][col] = null;
     }
 
     private static void displaySolutions() {
-        System.out.println("Solutions:");
         for (int[][] solution : solutions) {
             for (int[] row : solution) {
                 String rowStr = Arrays.stream(row)
                                       .mapToObj(Integer::toString)
-                                      .collect(Collectors.joining(", "));
+                                      .collect(Collectors.joining(" "));
                 System.out.println(rowStr);
             }
-            System.out.println(); // Print a blank line between solutions
+            System.out.println(); // blank line between solutions
         }
     }
 }
